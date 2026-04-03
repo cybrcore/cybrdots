a8013a804f6ccc1082ac40ff695b77bed3335958
Kevin Wulfric Scherrer <30331748+scherrer-txt@users.noreply.github.com>
2026-04-03 12:44:11 +0200

    refactor: replace nvim/ with submodule → cybrcore/cybr-nvim

--- nvim/colors/cybrvim.lua
+++
@@ -1,8 +0,0 @@
--- # ---------------------------------------
--- # CYBRvim     neovim theme (part of cybrland)
--- # Project:    https://github.com/scherrer-txt/cybrland
--- # Author:     scherrer-txt   |   License:     GPL-3.0
--- # Source:     ~/.config/nvim/colors/cybrvim.lua
--- # ---------------------------------------
-
-require('cybrvim').load()
